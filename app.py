29f49ba90242b741c0698be595295fd54cd0afd5
Danil Kozlov <danil@example.com>
2024-10-08 15:52:48 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
